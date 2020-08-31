3fc6d6f13e0752389bd4a45a1a0fa5cc48db81d9
Mike Wilson <mrmwilson@live.co.uk>
2020-08-31 16:43:05 +0100

    added get img function, added button -needs amending-

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("test")
+window.onload = function () {
+
+}
+
+function GetShibeImg() {
+
+    var randomShibe = fetch('https://cdn.shibe.online/shibes/25274238a3ff550a072cb9c9f6898ee77fbc6c84.jpg' + Image, {
+            method: 'GET'
+        })
+        .then(
+            response => response.json()
+        )
+        .then(
+            data => {
+                return data;
+            }
+        );
+
+    return randomShibe;
+
+}
