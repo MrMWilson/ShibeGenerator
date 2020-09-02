96573bdf679d088988e15ba06c39982d674543d0
Mike Wilson <mrmwilson@live.co.uk>
2020-09-02 19:44:17 +0100

    replaced API with working one

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ window.onload = function () {
 
 }
 
-function GetShibeImg() {
-    debugger;
+function GetShibeImg(pokedexNumber) {
 
-    var randomShibe = fetch('http://shibe.online/api/shibes')
+    var randomShibe = fetch('https://pokeapi.co/api/v2/pokemon-form/' + pokedexNumber + '/', {
+            method: 'GET'
+        })
+
         .then(
             response => response.json()
         )
@@ -15,13 +17,19 @@ function GetShibeImg() {
             }
         );
 
-    return randomShibe
+    return randomShibe;
 }
 
 async function DisplayShibe() {
 
     var shibe = document.getElementById("randomShibe");
-    var getRandomShibe = await GetShibeImg();
+    var randomNumber = GenerateRandomNumber(1, 151);
+    var getRandomShibe = await GetShibeImg(randomNumber);
 
-    shibe.src = getRandomShibe;
+    shibe.src = getRandomShibe.sprites.front_shiny;
+}
+
+function GenerateRandomNumber(lowerRange, upperRange) {
+    var randomNumber = Math.floor(Math.random() * (parseInt(upperRange) - parseInt(lowerRange) + 1) + parseInt(lowerRange));
+    return randomNumber;
 }
